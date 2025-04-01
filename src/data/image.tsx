e50c0d59599251fe6f52914861bfdee000ba221f
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-04-01 09:09:16 +0200

    update blur

--- src/data/image.tsx
+++ src/data/image.tsx
@@ -48,5 +48,155 @@ export const images = [
             "Il est l'auteur de la série de livres 'Icha Icha Paradise'",
             "Il a été le mentor de Naruto"
         ]
+    },
+    {
+        id: 6,
+        image: "https://staticg.sportskeeda.com/editor/2022/03/8b4b0-16478049972944-1920.jpg",
+        name: "Itachi",
+        hints: [
+            "Il est le frère aîné de Sasuke",
+            "Il a massacré tout son clan",
+            "Il était membre de l'Akatsuki"
+        ]
+    },
+    {
+        id: 7,
+        image: "https://static.wikia.nocookie.net/naruto/images/c/c3/Hinata.png",
+        name: "Hinata",
+        hints: [
+            "Elle appartient au clan Hyuga",
+            "Elle possède le Byakugan",
+            "Elle est amoureuse de Naruto depuis l'enfance"
+        ]
+    },
+    {
+        id: 8,
+        image: "https://sm.ign.com/t/ign_fr/screenshot/default/pain-les-chemins-de-la-douleur_6br6.1280.jpg",
+        name: "Pain",
+        hints: [
+            "Il est le chef de l'Akatsuki",
+            "Il possède le Rinnegan",
+            "Son vrai nom est Nagato"
+        ]
+    },
+    {
+        id: 9,
+        image: "https://static.wikia.nocookie.net/naruto/images/4/40/Gaara_Part_II.png",
+        name: "Gaara",
+        hints: [
+            "Il est le Kazekage du village de Suna",
+            "Il était le jinchūriki d'Ichibi",
+            "Il contrôle le sable"
+        ]
+    },
+    {
+        id: 10,
+        image: "https://sm.ign.com/ign_fr/screenshot/default/ob-8d06bb-naruto-shippuden-477-1080p-mkv-snapshot-08-25-2016-0_x7dq.jpg",
+        name: "Obito",
+        hints: [
+            "Il a prétendu être Madara Uchiha pendant longtemps",
+            "Il portait un masque orange en spirale",
+            "Il était membre de l'équipe de Minato"
+        ]
+    },
+    {
+        id: 11,
+        image: "https://static.wikia.nocookie.net/naruto/images/3/31/Tsunade_Infobox.png",
+        name: "Tsunade",
+        hints: [
+            "Elle est la Cinquième Hokage",
+            "Elle est l'une des trois ninjas légendaires",
+            "Elle est connue pour ses techniques médicales et sa force surhumaine"
+        ]
+    },
+    {
+        id: 12,
+        image: "https://naruto-official.com/common/images/jp/chara/chara_detail_orochimaru01.png",
+        name: "Orochimaru",
+        hints: [
+            "Il est l'un des trois ninjas légendaires",
+            "Il était obsédé par l'immortalité et les jutsu interdits",
+            "Il a fondé le village d'Oto"
+        ]
+    },
+    {
+        id: 13,
+        image: "https://static.wikia.nocookie.net/naruto/images/5/5c/Rock_Lee_Part_II.png",
+        name: "Rock Lee",
+        hints: [
+            "Il ne peut utiliser ni ninjutsu ni genjutsu",
+            "Il est spécialisé dans le taijutsu",
+            "Son sensei est Might Guy"
+        ]
+    },
+    {
+        id: 14,
+        image: "https://static.wikia.nocookie.net/naruto/images/2/21/Neji_Part_II.png",
+        name: "Neji",
+        hints: [
+            "Il appartient à la branche secondaire du clan Hyuga",
+            "Il possède le Byakugan",
+            "Il est le cousin de Hinata"
+        ]
+    },
+    {
+        id: 15,
+        image: "https://static.wikia.nocookie.net/naruto/images/6/64/Shikamaru_Part_II.png",
+        name: "Shikamaru",
+        hints: [
+            "Il est considéré comme un génie stratégique",
+            "Il utilise la technique de manipulation des ombres",
+            "Il trouve tout 'galère'"
+        ]
+    },
+    {
+        id: 16,
+        image: "https://static.wikia.nocookie.net/naruto/images/8/81/Kiba_Inuzuka_part_2.png",
+        name: "Kiba",
+        hints: [
+            "Il appartient au clan Inuzuka",
+            "Il a toujours son chien Akamaru avec lui",
+            "Il possède des capacités sensorielles accrues"
+        ]
+    },
+    {
+        id: 17,
+        image: "https://static.wikia.nocookie.net/naruto/images/8/8a/Choji_Part_2.png",
+        name: "Choji",
+        hints: [
+            "Il appartient au clan Akimichi",
+            "Il peut augmenter sa taille et sa masse",
+            "Il est toujours en train de manger des chips"
+        ]
+    },
+    {
+        id: 18,
+        image: "https://static.wikia.nocookie.net/naruto/images/a/a5/Ino_Part_II.png",
+        name: "Ino",
+        hints: [
+            "Elle appartient au clan Yamanaka",
+            "Elle peut prendre le contrôle de l'esprit de ses adversaires",
+            "Elle travaille dans une boutique de fleurs"
+        ]
+    },
+    {
+        id: 19,
+        image: "https://static.wikia.nocookie.net/naruto/images/2/27/Shino_timeskip.png",
+        name: "Shino",
+        hints: [
+            "Il appartient au clan Aburame",
+            "Il utilise des insectes pour combattre",
+            "Il porte toujours des lunettes noires"
+        ]
+    },
+    {
+        id: 20,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/AMG14_cosplayer_of_Minato_Namikaze_from_Naruto_20140809.jpg/1223px-AMG14_cosplayer_of_Minato_Namikaze_from_Naruto_20140809.jpg",
+        name: "Minato",
+        hints: [
+            "Il est le Quatrième Hokage",
+            "Il est connu sous le nom d'Éclair Jaune de Konoha",
+            "Il est le père de Naruto"
+        ]
     }
 ];
