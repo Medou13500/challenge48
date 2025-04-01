fdb07906c48e3e6540e3c9b507a5be25ba4ba0a5
PhantmVoid <108008538+PhntmVoid@users.noreply.github.com>
2025-04-01 08:46:59 +0200

    accueil update

--- src/Components/Accueil/accueil.tsx
+++ src/Components/Accueil/accueil.tsx
@@ -7,12 +7,10 @@ function Accueil() {
             backgroundImage: `url(${bgAkatsuki})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            backgroundRepeat: 'repeat',
+            backgroundRepeat: 'no-repeat',
             minHeight: '100vh',
             color: 'white',
             fontFamily: 'Arial, sans-serif',
-            textAlign: 'center',
-            padding: '20px'
         }}>
             <header className="d-flex justify-content-between align-items-center p-3" style={{ backgroundColor: 'rgba(36, 35, 35, 0.8)' }}>
                 <img src={logoAkatsuki} alt="Naruto Logo" style={{ height: '60px', width: '100px' }} />
@@ -24,7 +22,7 @@ function Accueil() {
                 </nav>
             </header>
             <main className="d-flex justify-content-center align-items-center" style={{ minHeight: '80vh' }}>
-                <div className="card p-5" style={{ backgroundColor: 'rgba(255, 255, 255)', borderRadius: '10px', maxWidth: '600px' }}>
+                <div className="card p-5" style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: '10px', maxWidth: '600px' }}>
                     <p className="lead">
                         Tu es un jeune ninja pris au piège dans un repaire de l'Akatsuki.
                         Pour t'échapper, tu dois résoudre plusieurs énigmes basées sur l'univers de Naruto.
@@ -38,7 +36,7 @@ function Accueil() {
                         Pour progresser, il faut trouver des indices et utiliser ses connaissances sur Naruto.
                     </p>
                     <div className="text-center mt-3">
-                        <button className="btn btn-light btn-lg" style={{ backgroundColor: 'rgba(216, 4, 4, 0.8)', borderRadius: '20px', padding: '10px 30px', fontWeight: 'bold' }}>
+                        <button className="btn btn-light btn-lg" style={{ backgroundColor: 'rgba(216, 4, 4, 0.84)', borderRadius: '20px', padding: '10px 30px', fontWeight: 'bold' }}>
                             Commencer le Jeu
                         </button>
                     </div>
